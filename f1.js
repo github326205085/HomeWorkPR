ac759918377d7b8ec2fd0c17ee055ea555dbe315
Efrat7248 <efrat7248@gmail.com>
2024-05-07 16:30:24 +0300

    add func to branch2

--- f1.js
+++ f1.js
@@ -2,3 +2,8 @@ const funcBranch2 = ()=>{
     alert("f1 from Branch2")
     return "Branch2"
 }
+
+const func = ()=>{
+    alert("func from Branch2")
+    return "func from Branch2 conflict number 2"
+}
